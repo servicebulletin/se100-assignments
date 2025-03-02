fc111d9adc2bebfb02d96f7fdb7440886f890ac4
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-03-02 12:38:16 +0800

    Question 1

--- assignment_4/question1.js
+++ assignment_4/question1.js
@@ -1,3 +1,12 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
-// Write your code below
+// Find highest value in array
+function max(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {max = arr[i]}
+    }
+    return max;
+}
+
+console.log("AMD 52-week high is %f", max(amdPrices))
