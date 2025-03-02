f0aae82be8bc7bf044ad5b559e127c518d94e33f
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-03-02 12:42:19 +0800

    Question 2

--- assignment_4/question2.js
+++ assignment_4/question2.js
@@ -1,3 +1,10 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
-// Write your code below
+// Find 7-day SMA
+function mean(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {sum += arr[i]}
+    return sum / arr.length
+}
+
+console.log("The 7-day SMA of AMD is %f", mean(amdPrices).toFixed(2))
