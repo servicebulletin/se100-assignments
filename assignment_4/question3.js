3d432ac75d371da835df71a1bc042d3e8391d7bd
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-03-07 09:02:32 +0800

    Question 3 complete

--- assignment_4/question3.js
+++ assignment_4/question3.js
@@ -1,3 +1,20 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
-// Write your code below
+// Find 7-day SMA
+function mean(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {sum += arr[i]}
+    return sum / arr.length
+}
+
+// Find number of days in which price was above 7-day SMA
+function daysAboveSMA(arr) {
+    let sma = mean(arr)
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > sma) {count++}
+    }
+    return count
+}
+
+console.log("Number of days AMD was above the 7-day SMA is: %i", daysAboveSMA(amdPrices))
