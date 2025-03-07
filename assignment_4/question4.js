5844211f356e1dbd3250f56355a510e501143313
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-03-07 10:08:59 +0800

    Question 4 complete

--- assignment_4/question4.js
+++ assignment_4/question4.js
@@ -4,3 +4,12 @@ const msftData = [
 ];
 
 // Write your code below
+function avgClosingPrice(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i][3];
+	}
+	return sum / arr.length;
+}
+
+console.log("Average closing price of MSFT is %f", avgClosingPrice(msftData).toFixed(2))
